b78c362d0d8955b74326ebb2a4a2a14fd438a945
MagnusSoderholm <magnus.soderholm@hotmail.com>
2025-05-22 14:20:42 +0200

    Update igen

--- WebbutvecklingLABB2.Client/WebbutvecklingLABB2.Client/Services/APIService.cs
+++ WebbutvecklingLABB2.Client/WebbutvecklingLABB2.Client/Services/APIService.cs
@@ -1,26 +1,4 @@
-﻿//using System.Net.Http.Json;
-//using WebbutvecklingLABB2.Shared.Models;
-
-
-//namespace WebbutvecklingLABB2.Client.Services
-//{
-//    public class ApiService
-//    {
-//        private readonly HttpClient _httpClient;
-
-//        public ApiService(HttpClient httpClient)
-//        {
-//            _httpClient = httpClient;
-//        }
-
-//        public Task<List<Product>> GetProductsAsync()
-//        {
-//            return _httpClient.GetFromJsonAsync<List<Product>>("api/products");
-//        }
-//    }
-//}
-
-using System.Net.Http;
+﻿using System.Net.Http;
 using System.Net.Http.Json;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -57,6 +35,23 @@ namespace WebbutvecklingLABB2.Client.Services
                 return null;
             }
         }
+
+        public async Task UpdateProductAsync(Product product)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"api/products/{product.Id}", product);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task<bool> DeleteProductAsync(int productId)
+        {
+            var response = await _httpClient.DeleteAsync($"api/products/{productId}");
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task AddProductAsync(Product product)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/products", product);
+            response.EnsureSuccessStatusCode();
+        }
     }
 }
-
